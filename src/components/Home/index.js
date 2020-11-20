8543e7789ede10888f6e708d3c1c3f412ca78f2a
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-11-20 12:08:13 +0800

    Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,54 +10,47 @@ import Circle from '../Backgrounds/circle';
 export default class Home extends Component {
 	render() {
 		return (
-			<div className="index-page">
+			<div className='index-page'>
 				<Navbar />
-				<div className="wrapper">
-					<div className="section section-hero section-shaped">
+				<div className='wrapper'>
+					<div className='section section-hero section-shaped'>
 						<Circle />
-						<div className="page-header">
-							<div className="container shape-container d-flex align-items-center py-lg">
-								<div className="col px-0">
-									<div className="row align-items-center justify-content-center">
-										<div className="col-lg-6 text-center">
+						<div className='page-header'>
+							<div className='container shape-container d-flex align-items-center py-lg'>
+								<div className='col px-0'>
+									<div className='row align-items-center justify-content-center'>
+										<div className='col-lg-6 text-center'>
 											<img
-												src="./assets/img/brand/white.png"
+												src='./assets/img/brand/white.png'
 												style={{ width: '200px' }}
-												className="img-fluid"
-												alt="Logo"
+												className='img-fluid'
+												alt='Logo'
 											/>
-											<p className="lead text-white">
-												Lorem ipsum dolor sit amet
-												consectetur, adipisicing elit.
-												Laudantium, hic? Laborum
-												voluptates ex, ullam molestias
-												aspernatur optio excepturi
-												dolores suscipit, quae pariatur
-												doloremque mollitia iusto
-												minima. Atque accusantium ut
-												laborum.
+											<p className='lead text-white'>
+												A place to find a school that
+												fits your needs.
 											</p>
-											<div className="btn-wrapper mt-5">
+											<div className='btn-wrapper mt-5'>
 												<Link
-													to="/schools"
-													className="btn btn-lg btn-white btn-icon mb-3 mb-sm-0"
+													to='/schools'
+													className='btn btn-lg btn-white btn-icon mb-3 mb-sm-0'
 												>
-													<span className="btn-inner--icon mb-1">
+													<span className='btn-inner--icon mb-1'>
 														<Search />
 													</span>
-													<span className="btn-inner--text">
+													<span className='btn-inner--text'>
 														Browse Schools
 													</span>
 												</Link>
 												<Link
-													to="/schools/add"
-													className="btn btn-lg btn-github btn-icon mb-3 mb-sm-0"
+													to='/schools/add'
+													className='btn btn-lg btn-github btn-icon mb-3 mb-sm-0'
 												>
-													<span className="btn-inner--icon mb-1">
+													<span className='btn-inner--icon mb-1'>
 														<Card />
 													</span>
-													<span className="btn-inner--text">
-														<span className="text-warning">
+													<span className='btn-inner--text'>
+														<span className='text-warning'>
 															Register
 														</span>{' '}
 														your School
@@ -69,18 +62,18 @@ export default class Home extends Component {
 								</div>
 							</div>
 						</div>
-						<div className="separator separator-bottom separator-skew zindex-100">
+						<div className='separator separator-bottom separator-skew zindex-100'>
 							<svg
-								x="0"
-								y="0"
-								viewBox="0 0 2560 100"
-								preserveAspectRatio="none"
-								version="1.1"
-								xmlns="http://www.w3.org/2000/svg"
+								x='0'
+								y='0'
+								viewBox='0 0 2560 100'
+								preserveAspectRatio='none'
+								version='1.1'
+								xmlns='http://www.w3.org/2000/svg'
 							>
 								<polygon
-									className="fill-white"
-									points="2560 0 2560 100 0 100"
+									className='fill-white'
+									points='2560 0 2560 100 0 100'
 								></polygon>
 							</svg>
 						</div>
--- src/components/Navbar/information.js
+++ src/components/Navbar/information.js
@@ -4,60 +4,58 @@ import { Link } from 'react-router-dom';
 export default class Information extends Component {
 	render() {
 		return (
-			<li className="nav-item dropdown">
+			<li className='nav-item dropdown'>
 				<a
-					href="/"
-					className="nav-link"
-					data-toggle="dropdown"
+					href='/'
+					className='nav-link'
+					data-toggle='dropdown'
 					onClick={(e) => e.preventDefault()}
-					role="button"
+					role='button'
 				>
-					<i className="ni ni-ui-04 d-lg-none"></i>
-					<span className="nav-link-inner--text">Information</span>
+					<i className='ni ni-ui-04 d-lg-none'></i>
+					<span className='nav-link-inner--text'>Information</span>
 				</a>
-				<div className="dropdown-menu dropdown-menu-xl">
-					<div className="dropdown-menu-inner">
+				<div className='dropdown-menu dropdown-menu-xl'>
+					<div className='dropdown-menu-inner'>
 						<Link
-							to="/schools"
-							className="media d-flex align-items-center"
+							to='/schools'
+							className='media d-flex align-items-center'
 						>
-							<div className="icon icon-shape bg-gradient-primary rounded-circle text-white">
-								<i className="ni ni-spaceship"></i>
+							<div className='icon icon-shape bg-gradient-primary rounded-circle text-white'>
+								<i className='ni ni-spaceship'></i>
 							</div>
-							<div className="media-body ml-3">
-								<h6 className="heading text-primary mb-md-1">
+							<div className='media-body ml-3'>
+								<h6 className='heading text-primary mb-md-1'>
 									Looking for a School?
 								</h6>
-								<p className="description d-none d-md-inline-block mb-0">
+								<p className='description d-none d-md-inline-block mb-0'>
 									Looking for suitable schools now made
 									easier.
 								</p>
 							</div>
 						</Link>
-						<a href="/" className="media d-flex align-items-center">
-							<div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-								<i className="ni ni-palette"></i>
+						<a href='/' className='media d-flex align-items-center'>
+							<div className='icon icon-shape bg-gradient-success rounded-circle text-white'>
+								<i className='ni ni-palette'></i>
 							</div>
-							<div className="media-body ml-3">
-								<h6 className="heading text-primary mb-md-1">
+							<div className='media-body ml-3'>
+								<h6 className='heading text-primary mb-md-1'>
 									Foundation
 								</h6>
-								<p className="description d-none d-md-inline-block mb-0">
-									Lorem ipsum dolor, sit amet consectetur
-									adipisicing elit. Debitis dolor esse amet
-									ullam doloremque, facere facilis.
+								<p className='description d-none d-md-inline-block mb-0'>
+									The foundations of schools in one.
 								</p>
 							</div>
 						</a>
-						<a href="/" className="media d-flex align-items-center">
-							<div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
-								<i className="ni ni-ui-04"></i>
+						<a href='/' className='media d-flex align-items-center'>
+							<div className='icon icon-shape bg-gradient-warning rounded-circle text-white'>
+								<i className='ni ni-ui-04'></i>
 							</div>
-							<div className="media-body ml-3">
-								<h5 className="heading text-warning mb-md-1">
+							<div className='media-body ml-3'>
+								<h5 className='heading text-warning mb-md-1'>
 									Interactive
 								</h5>
-								<p className="description d-none d-md-inline-block mb-0">
+								<p className='description d-none d-md-inline-block mb-0'>
 									Lorem ipsum dolor, sit amet consectetur
 									adipisicing elit. Debitis dolor esse amet
 									ullam doloremque, facere facilis.
